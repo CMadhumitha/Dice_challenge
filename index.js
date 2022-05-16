6d1e993f8e1caf907d6a6ad3b2edac99f255760a
CMadhumitha <71961906+CMadhumitha@users.noreply.github.com>
2022-05-16 22:18:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+console.log(randomNumber1);
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+console.log(randomNumber2);
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+
+// var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+// document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2)
+{
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else
+{
+    document.querySelector("h1").innerHTML = "Draw";
+}
